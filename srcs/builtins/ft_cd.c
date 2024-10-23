3356b50580ac6a15e1b6070ad3cf34fe189f66e4
Pacharapon Phuangtong <pphuangt@z3t1c4.42bangkok.com>
2024-10-23 17:16:17 +0700

    ft_cd

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -26,6 +26,37 @@ static void	ft_update_wd(char *new_wd, t_environ *environ, int i)
 	free(wd);
 }
 
+static void	print_err_cd(char *path)
+{
+	ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+	ft_putstr_fd(path, STDERR_FILENO);
+	ft_putendl_fd(": No such file or directory", STDERR_FILENO);
+}
+
+static int	go_back_path(char *old_pwd, char *last_pwd)
+{
+	if (old_pwd)
+	{
+		(void)last_pwd;
+		if (chdir(old_pwd) == -1)
+		{
+			print_err_cd(old_pwd);
+			return (0);
+		}
+		ft_putendl_fd(old_pwd, STDOUT_FILENO);
+	}
+	else
+	{
+		if (chdir(last_pwd) == -1)
+		{
+			print_err_cd(last_pwd);
+			return (0);
+		}
+		ft_putendl_fd(last_pwd, STDOUT_FILENO);
+	}
+	return (1);
+}
+
 static int	ft_change_dir(char *path, t_environ *environ)
 {
 	char		*pwd;
@@ -33,19 +64,22 @@ static int	ft_change_dir(char *path, t_environ *environ)
 	char		*old_pwd;
 	static char	last_pwd[MAXLINE];
 
+	if (!path || !ft_strncmp(path, "~", ft_strlen(path)))
+		chdir(get_variable_environ(0, "HOME", 4));
+	else if (!ft_strncmp(path, "-", ft_strlen(path)))
+	{
+		if (!go_back_path(get_variable_environ(0, "OLDPWD", 6), last_pwd))
+			return (1);
+	}
+	else if (chdir(path) == -1)
+	{
+		print_err_cd(path);
+		return (1);
+	}
 	old_pwd = get_variable_environ(0, "PWD", 3);
 	if (!old_pwd)
 		old_pwd = ft_strdup(last_pwd);
 	ft_update_wd(old_pwd, environ, 0);
-	if (!path)
-		chdir(get_variable_environ(0, "HOME", 4));
-	else if (chdir(path) == -1)
-	{
-		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-		ft_putstr_fd(path, STDERR_FILENO);
-		ft_putendl_fd(": No such file or directory", STDERR_FILENO);
-		return (1);
-	}
 	pwd = getcwd(buff, MAXLINE);
 	ft_update_wd(pwd, environ, 1);
 	ft_strlcpy(last_pwd, pwd, ft_strlen(pwd) + 1);
